695e0191b84a1624536f52c4d8a3a428fb039c29
Daiki <daqidiye@gmail.com>
2024-10-07 15:31:20 +0900

    fourth_commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,27 +12,29 @@ class OrdersController < ApplicationController
   def create
     ActiveRecord::Base.transaction do
       @order = current_user.orders.build(order_params)
-      begin
-        @order.ordered_lists.each do |ordered_list|
-          next if ordered_list.quantity == 0  # 注文数が0のアイテムはスキップ
-          item = Item.lock.find(ordered_list.item_id)
-          # 商品の注文数を加算する
-          item.add_ordered_quantity!(ordered_list.quantity)
-        end
 
-        if @order.save
-          redirect_to orders_path, notice: "注文が完了しました"
-        else
-          flash[:alert] = "注文の保存に失敗しました"
-          @items = Item.all.order(:created_at)  # @itemsを再取得
-          render :new
-        end
+      @order.ordered_lists.each do |ordered_list|
+        next if ordered_list.quantity == 0  # 注文数が0のアイテムはスキップ
 
-      rescue StandardError => e
-        flash[:alert] = e.message
-        @items = Item.all.order(:created_at)  # @itemsを再取得
-        render :new  # ページを再描画してエラーメッセージを表示
+        # 商品を悲観的ロックで取得
+        item = Item.lock.find(ordered_list.item_id)
+
+        # 在庫を安全に更新
+        item.add_ordered_quantity!(ordered_list.quantity)
       end
+
+      if @order.save
+        redirect_to orders_path, notice: "注文が完了しました"
+      else
+        flash[:alert] = "注文の保存に失敗しました"
+        @items = Item.all.order(:created_at)  # @itemsを再取得
+        render :new
+      end
+
+    rescue StandardError => e
+      flash[:alert] = e.message
+      @items = Item.all.order(:created_at)  # @itemsを再取得
+      render :new
     end
   end
 
