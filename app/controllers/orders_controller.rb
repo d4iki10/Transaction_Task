6a5ea2290f607c0b9166f38b864b6e000c82096f
Daiki <daqidiye@gmail.com>
2024-10-07 14:59:25 +0900

    third_commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,11 +10,30 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @order = current_user.orders.build(order_params)
-    @order.save
-    @order.update_total_quantity
-    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
-    redirect_to orders_path
+    ActiveRecord::Base.transaction do
+      @order = current_user.orders.build(order_params)
+      begin
+        @order.ordered_lists.each do |ordered_list|
+          next if ordered_list.quantity == 0  # 注文数が0のアイテムはスキップ
+          item = Item.lock.find(ordered_list.item_id)
+          # 商品の注文数を加算する
+          item.add_ordered_quantity!(ordered_list.quantity)
+        end
+
+        if @order.save
+          redirect_to orders_path, notice: "注文が完了しました"
+        else
+          flash[:alert] = "注文の保存に失敗しました"
+          @items = Item.all.order(:created_at)  # @itemsを再取得
+          render :new
+        end
+
+      rescue StandardError => e
+        flash[:alert] = e.message
+        @items = Item.all.order(:created_at)  # @itemsを再取得
+        render :new  # ページを再描画してエラーメッセージを表示
+      end
+    end
   end
 
   private
