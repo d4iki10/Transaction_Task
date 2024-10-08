e13664f5f819d3d98a1dbc0563ba5101dc821906
Daiki <daqidiye@gmail.com>
2024-10-08 12:20:08 +0900

    fifth_commit

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,32 +10,11 @@ class OrdersController < ApplicationController
   end
 
   def create
-    ActiveRecord::Base.transaction do
-      @order = current_user.orders.build(order_params)
-
-      @order.ordered_lists.each do |ordered_list|
-        next if ordered_list.quantity == 0  # 注文数が0のアイテムはスキップ
-
-        # 商品を悲観的ロックで取得
-        item = Item.lock.find(ordered_list.item_id)
-
-        # 在庫を安全に更新
-        item.add_ordered_quantity!(ordered_list.quantity)
-      end
-
-      if @order.save
-        redirect_to orders_path, notice: "注文が完了しました"
-      else
-        flash[:alert] = "注文の保存に失敗しました"
-        @items = Item.all.order(:created_at)  # @itemsを再取得
-        render :new
-      end
-
-    rescue StandardError => e
-      flash[:alert] = e.message
-      @items = Item.all.order(:created_at)  # @itemsを再取得
-      render :new
-    end
+    @order = current_user.orders.build(order_params)
+    @order.save
+    @order.update_total_quantity
+    # update_total_quantityメソッドは、注文された発注量を総量に反映するメソッドであり、Orderモデルに定義されています。
+    redirect_to orders_path
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,28 +2,4 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :total_quantity, presence: true
   has_many :orders, through: :ordered_lists
-
-  # 在庫を減らすメソッド
-  def reduce_stock!(quantity)
-    return if quantity <= 0
-
-    self.class.transaction do
-      item = Item.lock.find(self.id)  # 悲観的ロックを適用してアイテムをロック
-      if item.total_quantity >= quantity
-        item.total_quantity += quantity
-        item.save!
-      else
-        raise StandardError, "#{item.name}の在庫が足りません（残り#{item.total_quantity}個）"
-      end
-    end
-  end
-
-  # 注文数を加算するメソッド
-  def add_ordered_quantity!(quantity)
-    self.class.transaction do
-      item = Item.lock.find(self.id)  # 悲観的ロックを適用
-      item.total_quantity += quantity  # total_quantityに注文数を加算
-      item.save!
-    end
-  end
 end
